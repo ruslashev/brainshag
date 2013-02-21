8349c15f348f3fa82f514c0c1dbf15d970f4398b
ruslashev <ruslashev@gmail.com>
2013-02-21 21:11:15 +0400

    Using menus for choices

--- brainshag.cpp
+++ brainshag.cpp
@@ -1,56 +1,99 @@
-#include <iostream>
-#include <ncurses.h>
+#include "brainshag.hpp"
 
-// Bracketless Hello world: 	++++++++++>+++++++>++++++++++>+++>+<<<<->+++++++>++++++++++>+++>+<<<<->+++++++>++++++++++>+++>+<<<<->+++++++>++++++++++>+++>+<<<<->+++++++>++++++++++>+++>+<<<<->+++++++>++++++++++>+++>+<<<<->+++++++>++++++++++>+++>+<<<<->+++++++>++++++++++>+++>+<<<<->+++++++>++++++++++>+++>+<<<<->+++++++>++++++++++>+++>+<<<<->++.>+.+++++++..+++.>++.<<+++++++++++++++.>.+++.------.--------.>+.>.
-// Hello world: 				++++++++++[>+++++++>++++++++++>+++>+<<<<-]>++.>+.+++++++..+++.>++.<<+++++++++++++++.>.+++.------.--------.>+.>.
-
-int windowSizeX, windowSizeY;
-int choice(std::string info, int numOfChoices, int moveY)
+int choice(vector<string> choices, int moveY)
 {
+	// > vector
+	// I am sorry
+	const int nchoices = choices.size();
+	ITEM* items[nchoices];
+	MENU* menu = NULL;
+
+	for(int i = 0; i < nchoices; i++) {
+		items[i] = new_item(const_cast<char*>(choices[i].c_str()), "");
+	}
+	items[nchoices] = (ITEM*)NULL;
+
+	menu = new_menu((ITEM**)items);
+	move(moveY, 0);
+	post_menu(menu);
+	refresh();
+
 	int ch;
-	mvprintw(moveY, 0, info.c_str());
-	mvprintw(moveY+numOfChoices+1, 0, "$ ");
-	// menus are for sissies
-	while ((ch = getch()-48) < 1 || ch > numOfChoices) {
-		mvprintw(moveY+numOfChoices+1, 0, "I thought I can trust you dude\n");
-		mvprintw(moveY+numOfChoices+2, 0, "$ ");
+	while(ch = getch()) {
+		switch (ch) {
+			case KEY_DOWN:
+				menu_driver(menu, REQ_DOWN_ITEM);
+				break;
+			case KEY_UP:
+				menu_driver(menu, REQ_UP_ITEM);
+				break;
+			case 10: // enter
+				return item_index(current_item(menu)) + 1;
+				break;
+		}
 	}
 
-	return ch;
+	unpost_menu(menu);
+	free_menu(menu);
+	for (int i = 0; i < nchoices; i++)
+		free_item(items[i]);
 }
 
 int main(int argc, char *argv[])
 {
-	initscr(); raw(); keypad(stdscr, true); noecho(); cbreak();
+	initscr(); keypad(stdscr, true); cbreak();
 	getmaxyx(stdscr, windowSizeY, windowSizeX);
 	
-	int ch;
 	printw("Welcome to Brainshag, a visual brainfuck interpreter!");
-	ch = choice("Would you like to:"														"\n"
-				"1) Simply execute some brainfuck"											"\n"
-				"2) Load a super-amazing visual IDE or something that is not even done yet"	"\n", 2, 2);
+	mvprintw(1, 0, "Would you like to:");
+	vector<string> t;
+	t.push_back("1) Simply execute some brainfuck");
+	t.push_back("2) Load an IDE or something that is not even done yet");
+	switch (choice(t, 3)) {
+		case 1:
+			simpleBF();
+			break;
+
+		case 2:
+			clear();
+			mvprintw(0, 0, "Not implemented yet.");
+	}
 
 	getch();
 	endwin();
+	
+	return 0;
+}
 
-	/*
+void simpleBF()
+{
+	/*char code[1000];
+	
+	clear();
+	printw("Enter your code:\n");
+	getstr(code);
+	
+	clear();
+	attron(A_REVERSE);
+	printw("Program output: ");
+	attroff(A_REVERSE);
+	chgat(-1, A_REVERSE, 0, NULL);
+	move(1, 0);
+
 	char mem[30000] = {0};
-	std::string code;
-	std::cin >> code;
 	int ptr = 0;
 	int openBrackets = 0;
 
-	for (int i = 0; i < code.length(); i++)
-	{
+	for (int i = 0; i < strlen(code); i++) {
 		char ch = code[i];
 		
 		ptr += ch == '>' ? 1 : ch == '<' ? -1 : 0;
 		mem[ptr] += ch == '+' ? 1 : ch == '-' ? -1 : 0;
 
 		if (ch ==  '.')
-			std::cout << mem[ptr];
+			addch(mem[ptr]);
 		else if (ch == ',')
-			std::cin >> mem[ptr];
+			mem[ptr] = getch();
 		else if (ch == '[') {
 			if (mem[ptr] == 0) {
 				i++;
@@ -69,8 +112,5 @@ int main(int argc, char *argv[])
 			}
 			i--;
 		}
-	}
-	*/
-
-	return 0;
+	}*/
 }
---
+++ makefile
@@ -0,0 +1,2 @@
+default: brainshag.hpp
+	g++ brainshag.cpp -std=c++0x -g -o brainshag -lncurses -lmenu
