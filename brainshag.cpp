a69f64c89b3b2a7ac5c1e6c90cc6a27962a304b9
Ruslan Akbashev <ruslashev@gmail.com>
2013-04-20 20:51:40 +0400

    First UI draft done
    It's not working yet but it works with any size screen

--- brainshag.cpp
+++ brainshag.cpp
@@ -40,8 +40,38 @@ void loadInterpreter()
 	 */
 
 	refresh();
-	WINDOW *tape = newwin(3, WindowSizeX, 0, 0);
-	box(tape, 0, 0);
-	wrefresh(tape);
+
+	int *tape, tapeSize;
+	tapeSize = 11;
+	tape = (int*)calloc(tapeSize, sizeof(int));
+	if (tape == NULL)
+	{
+		// well shit
+		// TODO handle this
+		endwin();
+		exit(1);
+	}
+
+	WINDOW *tapeWin = newwin(3, tapeSize*4+1, 0, 1);
+	wborder(tapeWin, '|', '|', '-', '-', '+', '+', '+', '+');
+	wmove(tapeWin, 1, 1);
+	for (int i = 0; i < tapeSize; i++)
+	{
+		// TODO make a pretty separator
+		// i.e.: +
+		//       |
+		//       +
+		wprintw(tapeWin, " %d |", tape[i]);
+	}
+	wrefresh(tapeWin);
+
+	mvaddstr(WindowSizeY-4, 1, "Input: ");
+	mvaddstr(WindowSizeY-3, 1, "Output: ");
+
+	WINDOW *editorWin = newwin(WindowSizeY-4-4, WindowSizeX-3, 4, 1);
+	box(editorWin, 0, 0);
+	wrefresh(editorWin);
+
+	free(tape);
 }
 
