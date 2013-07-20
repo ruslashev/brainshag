d530c292fd94a31ddd470f161630b38207267532
Ruslan Akbashev <ruslashev@gmail.com>
2013-07-20 20:45:59 +0400

    Misc improvements

--- brainshag.cpp
+++ brainshag.cpp
@@ -14,19 +14,18 @@ int main(int argc, char *argv[])
 	loadInterpreter();
 
 	endwin();
-	printf("Bye!");
+	printf("Bye!\n");
 	return 0;
 }
 
-Tape::Tape(int newSize)
+Tape::Tape()
 {
-	size = newSize;
-	dataPointerPos = 0;
+	size = 1;
+	dataPointer = 0;
 	data = (int*)calloc(size, sizeof(int));
 	if (data == NULL) {
-		// well shit
-		// TODO handle this
 		endwin();
+		printf("Failed to allocate memory for tape");
 		exit(1);
 	}
 
@@ -41,7 +40,7 @@ void Tape::Update()
 		wprintw(window, "%4d|", data[i]);
 
 	mvchgat(3, 0, -1, A_INVIS, 0, NULL); // clear the line
-	mvaddch(3, dataPointerPos*5+4, '^');
+	mvaddch(3, dataPointer*5+4, '^');
 	refresh();
 
 	wrefresh(window);
@@ -70,7 +69,8 @@ void loadInterpreter()
 	 * Hello, W
 	 */
 
-	Tape tape(11);
+	Tape tape;
+	tape.Update();
 
 	mvaddstr(WindowSizeY-4, 1, "Input: ");
 	mvaddstr(WindowSizeY-3, 1, "Output: ");
@@ -83,15 +83,58 @@ void loadInterpreter()
 	wrefresh(editorWinBorder);
 
 	WINDOW *editorWin = newwin(WindowSizeY-4-6, WindowSizeX-4, 5, 2);
-	string code;
+	char ch;
+	std::string buffer;
 	scrollok(editorWin, true);
-	while (1) // TODO that's terrible to call update that often
+	while (true)
 	{
-		tape.Update();
+		ch = wgetch(editorWin);
 
-		code = wgetch(editorWin);
-		waddstr(editorWin, code.c_str());
+		if (ch == 27)
+		{
+
+		}
+
+		buffer += ch;
+		waddch(editorWin, ch);
+		tape.Update();
 		wrefresh(editorWin);
 	}
 }
 
+// void simpleInterpreter(std::string code)
+// {
+// 	char mem[30000] = {0};
+// 	int dataPtr = 0;
+// 	int openBrackets = 0;
+//
+// 	for (int i = 0; i < code.size(); i++)
+// 	{
+// 		char ch = code[i];
+//
+// 		ptr += ch == '>' ? 1 : ch == '<' ? -1 : 0;
+// 		mem[ptr] += ch == '+' ? 1 : ch == '-' ? -1 : 0;
+//
+// 		if (ch ==  '.')
+// 			addch(mem[ptr]);
+// 		else if (ch == ',')
+// 			mem[ptr] = getch();
+// 		else if (ch == '[') {
+// 			if (mem[ptr] == 0) {
+// 				i++;
+// 				while (openBrackets > 0 || code[i] != ']') {
+// 					if (code[i] == '[') openBrackets++;
+// 					if (code[i] == ']') openBrackets--;
+// 					i++;
+// 				}
+// 			}
+// 		} else if (ch == ']') {
+// 			i--;
+// 			while (openBrackets > 0 || code[i] != '[') {
+// 				if (code[i] == ']') openBrackets++;
+// 				if (code[i] == '[') openBrackets--;
+// 				i--;
+// 			}
+// 			i--;
+// 		}
+// 	}
--- brainshag.hpp
+++ brainshag.hpp
@@ -5,8 +5,6 @@
 // Bracketless Hello world: 	++++++++++>+++++++>++++++++++>+++>+<<<<->+++++++>++++++++++>+++>+<<<<->+++++++>++++++++++>+++>+<<<<->+++++++>++++++++++>+++>+<<<<->+++++++>++++++++++>+++>+<<<<->+++++++>++++++++++>+++>+<<<<->+++++++>++++++++++>+++>+<<<<->+++++++>++++++++++>+++>+<<<<->+++++++>++++++++++>+++>+<<<<->+++++++>++++++++++>+++>+<<<<->++.>+.+++++++..+++.>++.<<+++++++++++++++.>.+++.------.--------.>+.>.
 // Hello world: 				++++++++++[>+++++++>++++++++++>+++>+<<<<-]>++.>+.+++++++..+++.>++.<<+++++++++++++++.>.+++.------.--------.>+.>.
 
-using namespace std;
-
 void loadInterpreter();
 
 class Tape
@@ -15,62 +13,11 @@ public:
 	WINDOW *window;
 	int size;
 	int *data;
-	int dataPointerPos;
+	int dataPointer;
 
 	void Update();
 
-	Tape(int newSize);
+	Tape();
 	~Tape();
 };
 
-/*
-void simpleBF()
-{
-	clear();
-	char code[1000];
-	
-	clear();
-	printw("Enter your code:\n");
-	getstr(code);
-	
-	clear();
-	attron(A_REVERSE);
-	printw("Program output: ");
-	attroff(A_REVERSE);
-	chgat(-1, A_REVERSE, 0, NULL);
-	move(1, 0);
-
-	char mem[30000] = {0};
-	int ptr = 0;
-	int openBrackets = 0;
-
-	for (int i = 0; i < strlen(code); i++) {
-		char ch = code[i];
-		
-		ptr += ch == '>' ? 1 : ch == '<' ? -1 : 0;
-		mem[ptr] += ch == '+' ? 1 : ch == '-' ? -1 : 0;
-
-		if (ch ==  '.')
-			addch(mem[ptr]);
-		else if (ch == ',')
-			mem[ptr] = getch();
-		else if (ch == '[') {
-			if (mem[ptr] == 0) {
-				i++;
-				while (openBrackets > 0 || code[i] != ']') {
-					if (code[i] == '[') openBrackets++;
-					if (code[i] == ']') openBrackets--;
-					i++;
-				}
-			}
-		} else if (ch == ']') {
-			i--;
-			while (openBrackets > 0 || code[i] != '[') {
-				if (code[i] == ']') openBrackets++;
-				if (code[i] == '[') openBrackets--;
-				i--;
-			}
-			i--;
-		}
-	}
- */
